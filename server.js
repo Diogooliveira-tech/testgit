695f487899ac7ebcf47741240802b82ab1cc0812
Diogo Oliveira <64809616+Diogooliveira-tech@users.noreply.github.com>
2020-10-12 11:12:42 -0300

    Update server.js

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ app.get('/', (req, res) => {
     res.status(200).send({message: 'Olá mundo estou funcionando!'});
 })
 
+app.get('/olá', (req, res) => {
+    res.status(200).send({message: 'Oi eu sou a rota Olá!'});
+})
+
 app.listen(3001, () => {
     console.log('Api rodando na porta 3001');
 })
